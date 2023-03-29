9549688a419d6b28e29b70f1cdea82295716908f
Alexander Hendorf <ah@koenigsweg.com>
2023-03-29 17:27:21 +0200

    debugged

--- app/routers/tickets.py
+++ app/routers/tickets.py
@@ -2,16 +2,16 @@ from fastapi import APIRouter
 
 from app.tito.tito_api import get_all_tickets
 
-router = APIRouter(prefix="/tickets", tags=["bem", "pa"])
+router = APIRouter(prefix="/tickets", tags=["Attendees"])
 
 
 @router.get("/refresh_all")
 async def refresh_all():
     get_all_tickets(from_cache=False)
+    # TODO save all ticket info in memory
     return 200
 
 
 @router.get("/ticket/{tito_id}")
 async def get_ticket_by_id():
-    get_all_tickets(from_cache=False)
-    return 200
+
