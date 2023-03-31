472795a0332693d1843e8aa1357acafdcce4cd78
Alexander Hendorf <ah@koenigsweg.com>
2023-03-31 10:53:16 +0200

    resolves #2

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -24,6 +24,8 @@ account_slug = CONFIG["account_slug"]
 event_slug = CONFIG["event_slug"]
 
 token_path = project_root / "_private/TOKEN.txt"
+if not token_path.exists():
+    token_path.open("w").write("PUT-YOUR-TITO-TOKEN-HERE")
 TOKEN = token_path.open().read()
 
 __all__ = ["CONFIG", "TOKEN", "event_slug", "account_slug"]
